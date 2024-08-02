16d4e3d10d3aa4d8c6ad2405c9a9a82fb64e43cb
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-08-02 21:02:34 +0600

    test

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -13,19 +13,18 @@ const Book = () => {
       content: page?.htmlcontent,
    }));
    console.log(bookData);
-   // Assuming `leftPageIndex` and `rightPageIndex` are controlled via state
+
    const [leftPageIndex, setLeftPageIndex] = useState(0);
    const [rightPageIndex, setRightPageIndex] = useState(1);
    const [canGoToNextPage, setCanGoToNextPage] = useState(true);
 
 
-   // Refs for page containers
    const leftContainerRef = useRef(null);
    const rightContainerRef = useRef(null);
 
    return (
       <div className="h-screen grid grid-rows-12 md:grid-rows-12">
-         {/* Main Content Area (spanning 10 rows on md and above, 9 rows below md) */}
+         {/* 10 rows for big devices, 9 rows for md and lower devices */}
          <div className="row-span-9 md:row-span-10 bg-gray-200">
             <div className="h-full flex items-center justify-center">
                <div className="flex-1 pb-4 flex justify-center">
@@ -47,14 +46,14 @@ const Book = () => {
             </div>
          </div>
 
-         {/* Second Section (spanning 1 row on all devices) */}
+         {/* 1 row for pagination and it will stay same */}
          <div className="row-span-1 bg-blue-200">
             <div className="h-full flex items-center justify-center">
                <p className="text-lg">Second Section (1 row)</p>
             </div>
          </div>
 
-         {/* Third Section (spanning 2 rows below md, 1 row on md and above) */}
+         {/*it will take 2 row and 1 row depending on the device sizes*/}
          <div className="row-span-2 md:row-span-1 bg-[#FFDFCD]">
             <div className="h-full flex items-center justify-center">
                <Controls />
